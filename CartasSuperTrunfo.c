6a8663a289ae2291c457bd61f23e6086281d9806
Copete-99 <cmatheus.silva@outlook.com>
2025-08-04 22:59:46 +0000

    comparando 01

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,61 +2,66 @@
 
 
 int main() {
-    char cidade[20];
-    float PIB;
-    int populacao;
-    int pontoturistico;
-    float area;
-    char estado[10];
-    int codigo;
-    char nome[50];
-
+    char cidade1[20];
+    char cidade2[20];
+    float PIB1;
+    float PIB2;
+    int populacao1;
+    int populacao2;
+    int pontoturistico1;
+    int pontoturistico2;
+    float area1;
+    float area2;
+    char estado1[10];
+    char estado2[10];
+    int codigo1;
+    int codigo2;
     
-    
-
     printf ("Desafio Super Trunfo! \n ");
     
-    printf ("Digite o estado:\n");
-    scanf ("%s",estado);
+    printf ("Digite o estado 01:\n");
+    scanf ("%s",estado1);
     
-    printf ("Digite o estado:\n");
-    scanf ("%s",estado);
+    printf ("Digite o estado 02:\n");
+    scanf ("%s",estado2);
     
-    printf ("Codigo:\n");
-    scanf ("%d", &codigo);
+    printf ("Codigo 01:\n");
+    scanf ("%d", &codigo1);
     
-    printf ("Codigo:\n");
-    scanf ("%d", &codigo);
+    printf ("Codigo 02:\n");
+    scanf ("%d", &codigo2);
     
-    printf ("Nome da cidade:\n");
-    scanf ("%s",cidade);
+    printf ("Nome da cidade 01:\n");
+    scanf ("%s",cidade1);
     
-    printf ("Nome da cidade:\n");
-    scanf ("%s",cidade);
+    printf ("Nome da cidade 02:\n");
+    scanf ("%s",cidade2);
     
-    printf ("População:\n");
-    scanf ("%d",&populacao);
+    printf ("População 01:\n");
+    scanf ("%d",&populacao1);
 
-    printf ("População:\n");
-    scanf ("%d",&populacao);
+    printf ("População 02:\n");
+    scanf ("%d",&populacao2);
 
-    printf ("Area:\n");
-    scanf ("%f", &area);
+    printf ("Area 01:\n");
+    scanf ("%f", &area1);
     
-    printf ("Area:\n");
-    scanf ("%f", &area);
+    printf ("Area 02:\n");
+    scanf ("%f", &area2);
    
-    printf ("PIB:\a");
-    scanf ("%d", &PIB);
+    printf ("PIB 01:\a");
+    scanf ("%d", &PIB1);
 
-    printf ("PIB:\a");
-    scanf ("%d", &PIB);
+    printf ("PIB 02:\a");
+    scanf ("%d", &PIB2);
     
-    printf ("Ponto turisticos:\n");
-    scanf ("%d", &pontoturistico);
+    printf ("Ponto turisticos 01:\n");
+    scanf ("%d", &pontoturistico1);
 
-    printf ("Ponto turisticos:\n");
-    scanf ("%d", &pontoturistico);
+    printf ("Ponto turisticos 02:\n");
+    scanf ("%d", &pontoturistico2);
+
+    printf ("Carta 01: estado1 %s - codigo01 %d - cidade1 %s - populacao %d - area1 %f - PIB1 %d - pontoturistico1 %d ")
 
     
     
