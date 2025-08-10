243cca4c163ec754266a3390ace5a70c865234b9
Copete-99 <cmatheus.silva@outlook.com>
2025-08-10 14:47:55 +0000

    CORREÇÃONOVATOFINAL

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,14 +4,10 @@
 int main() {
     char cidade1[20];
     char cidade2[20];
-    float PIB1;
-    float PIB2;
-    int populacao1;
-    int populacao2;
-    int pontoturistico1;
-    int pontoturistico2;
-    float area1;
-    float area2;
+    float PIB1, PIB2;
+    int populacao1, populacao2;
+    int pontoturistico1, pontoturistico2;
+    float area1, area2;
     char estado1[10];
     char estado2[10];
     char codigo1 [4];
@@ -68,8 +64,8 @@ int main() {
     printf ("Código 01: %s \n", codigo1);
     printf ("Cidade 01: %s\n", cidade1);
     printf ("População 01: %d \n", populacao1);
-    printf ("Area 01: %2.f \n", area1);
-    printf ("PIB 01: %2.f \n", PIB1);
+    printf ("Area 01: %2.f km²\n", area1);
+    printf ("PIB 01: %2.f bilhoes\n", PIB1);
     printf ("Ponto turistico 01: %d \n", pontoturistico1);
   
 
@@ -79,8 +75,8 @@ int main() {
     printf ("Código 02: %s \n", codigo2);
     printf ("Cidade 02: %s\n", cidade2);
     printf ("População 02: %d \n", populacao2);
-    printf ("Area 02: %2.f \n", area2);
-    printf ("PIB 02: %2.f \n", PIB2);
+    printf ("Area 02: %2.f km²\n", area2);
+    printf ("PIB 02: %2.f bilhoes\n", PIB2);
     printf ("Ponto turistico 02: %d \n", pontoturistico2);
   
 
